272dcfc73a6e9775eec2b7e2dda699642e288cc6
Ashrith Sagar <ashrith9sagar@gmail.com>
2026-02-17 06:58:11 +0530

    ✅ NDArray

--- tests/test_ndarray_3.py
+++ tests/test_ndarray_3.py
@@ -6,56 +6,54 @@ Extended Tests for TypedNDArray - 3
 # pyright: reportGeneralTypeIssues = false
 # pyright: reportInvalidTypeArguments = false
 
-from typing import Literal, TypeVar
+from typing import TypeVar
 
 import numpy as np
 import pytest
 
 from typed_numpy._typed.context import enforce_shapes
+from typed_numpy._typed.helpers import FIVE, THREE, TWO
 from typed_numpy._typed.ndarray import DimensionError, ShapeError, TypedNDArray
+
+N = TypeVar("N", bound=int, default=int)
+M = TypeVar("M", bound=int, default=int)
 
 
 class TestRepeatedTypeVarConsistency:
     def test_square_matrix_success(self) -> None:
-        N = TypeVar("N", bound=int, default=int)
         Square = TypedNDArray[tuple[N, N]]
         arr = Square(np.ones((4, 4)))
         assert arr.shape == (4, 4)
 
     def test_square_matrix_failure(self) -> None:
-        N = TypeVar("N", bound=int, default=int)
         Square = TypedNDArray[tuple[N, N]]
         with pytest.raises(ShapeError):
             Square(np.ones((3, 4)))
 
 
 class TestLiteralAndTypeVarMix:
-    def test_literal_and_typevar_reuse(self):
-        N = TypeVar("N")
-        Array = TypedNDArray[tuple[Literal[2], N, N]]  # type: ignore
+    def test_literal_and_typevar_reuse(self) -> None:
+        Array = TypedNDArray[tuple[TWO, N, N]]
         arr = Array(np.ones((2, 3, 3)))
         assert arr.shape == (2, 3, 3)
 
-    def test_literal_and_typevar_reuse_failure(self):
-        N = TypeVar("N")
-        Array = TypedNDArray[tuple[Literal[2], N, N]]  # type: ignore
+    def test_literal_and_typevar_reuse_failure(self) -> None:
+        Array = TypedNDArray[tuple[TWO, N, N]]
         with pytest.raises(ShapeError):
             Array(np.ones((2, 3, 4)))
 
 
 class TestMethodLevelIsolation:
-    def test_method_level_does_not_persist(self):
-        M = TypeVar("M")
-
+    def test_method_level_does_not_persist(self) -> None:
         class Model:
             @enforce_shapes
-            def foo(self, x: TypedNDArray[tuple[M]]) -> TypedNDArray[tuple[M]]:  # type: ignore
+            def foo(self, x: TypedNDArray[tuple[M]]) -> TypedNDArray[tuple[M]]:
                 return x
 
         m = Model()
 
-        a = TypedNDArray[tuple[5]]([1, 2, 3, 4, 5])
-        b = TypedNDArray[tuple[3]]([1, 2, 3])
+        a = TypedNDArray[tuple[FIVE]]([1, 2, 3, 4, 5])
+        b = TypedNDArray[tuple[THREE]]([1, 2, 3])
 
         m.foo(a)
         # Should not conflict with previous call
@@ -63,24 +61,22 @@ class TestMethodLevelIsolation:
 
 
 class TestReturnValidation:
-    def test_return_shape_violation(self):
-        N = TypeVar("N")
-
+    def test_return_shape_violation(self) -> None:
         class Model:
             @enforce_shapes
-            def foo(self, x: TypedNDArray[tuple[N]]) -> TypedNDArray[tuple[N]]:  # type: ignore
+            def foo(self, x: TypedNDArray[tuple[N]]) -> TypedNDArray[tuple[N]]:
                 return TypedNDArray([1, 2, 3])  # always size 3
 
         m = Model()
 
-        a = TypedNDArray[tuple[5]]([1, 2, 3, 4, 5])
+        a = TypedNDArray[tuple[FIVE]]([1, 2, 3, 4, 5])
 
         with pytest.raises(DimensionError):
             m.foo(a)
 
 
 class TestZeroDimIteration:
-    def test_zero_dim_not_iterable(self):
+    def test_zero_dim_not_iterable(self) -> None:
         Scalar = TypedNDArray[tuple[()]]
         arr = Scalar(5)
 
@@ -89,15 +85,15 @@ class TestZeroDimIteration:
 
 
 class TestSubclassPreservation:
-    def test_view_preserves_type(self):
-        Array3 = TypedNDArray[tuple[3]]
+    def test_view_preserves_type(self) -> None:
+        Array3 = TypedNDArray[tuple[THREE]]
         arr = Array3([1, 2, 3])
         view = arr.view()
 
         assert isinstance(view, TypedNDArray)
 
-    def test_slice_preserves_type(self):
-        Array5 = TypedNDArray[tuple[5]]
+    def test_slice_preserves_type(self) -> None:
+        Array5 = TypedNDArray[tuple[FIVE]]
         arr = Array5([1, 2, 3, 4, 5])
         sliced = arr[1:]
 
@@ -105,10 +101,10 @@ class TestSubclassPreservation:
 
 
 class TestDTypeTypeVar:
-    def test_dtype_typevar_binding(self):
+    def test_dtype_typevar_binding(self) -> None:
         T = TypeVar("T", bound=np.generic)
 
-        Array = TypedNDArray[tuple[3], np.dtype[T]]
+        Array = TypedNDArray[tuple[THREE], np.dtype[T]]
         Array_i32 = Array[np.int32]
 
         arr = Array_i32([1, 2, 3])
