8e763a4db3de864b44eb951632e77eb594f6bf34
Ashrith Sagar <ashrith9sagar@gmail.com>
2026-02-20 08:13:49 +0530

    🎨 Debug

--- src/typed_numpy/_typed/_debug.py
+++ src/typed_numpy/_typed/_debug.py
@@ -19,15 +19,19 @@ def diagnostic(obj: Any, pfx: str | None = None) -> Tree:
         case tuple():
             for x in obj:  # type: ignore
                 tree.add(diagnostic(x))
+
         case TypeVar():
             tree.add(diagnostic(obj.__bound__, "__bound__:"))
             tree.add(diagnostic(obj.__constraints__, "__constraints__:"))
             tree.add(diagnostic(obj.__default__, "__default__:"))
             tree.add(diagnostic(obj.__covariant__, "__covariant__:"))
             tree.add(diagnostic(obj.__contravariant__, "__contravariant__:"))
-        case _ if (origin := get_origin(obj)) is not None:  # GenericAlias | Literal
+
+        # GenericAlias | Literal | UnionType
+        case _ if (origin := get_origin(obj)) is not None:
             tree.add(diagnostic(origin, "origin:"))
             tree.add(diagnostic(get_args(obj), "args:"))
+
         case _:
             pass
     return tree
