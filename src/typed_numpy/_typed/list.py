6e4dce182695a90f904caf8509ac3b6156de52fa
Ashrith Sagar <ashrith9sagar@gmail.com>
2026-02-27 22:14:54 +0530

    👔 List

--- src/typed_numpy/_typed/list.py
+++ src/typed_numpy/_typed/list.py
@@ -6,7 +6,7 @@ TypedList
 
 import copy
 import numbers
-from types import GenericAlias
+from types import GenericAlias, NoneType, UnionType
 from typing import (
     Any,
     Callable,
@@ -70,27 +70,88 @@ def _is_assignable(value: Any, expected_type: type) -> bool:
         return False
 
 
-def _validate_length(object: Sequence[Item], length: int | TypeVar | None) -> None:
-    if not TypedListConfig.VALIDATE_LENGTH:
-        return None
+def _resolve_length(length: Any) -> Any:
+    # ~TypeVar
+    if isinstance(length, TypeVar):
+        # [TODO]: How should typing.NoDefault be handled?
+        length = _resolve_length(length.__default__)
 
     origin = get_origin(length)
 
-    # Literal[N]
+    # Literal[A, B, ...]
     if origin is Literal:
-        # [TODO] How should we handle multiple args case?
-        length = get_args(length)[0]
+        length = set(get_args(length))
 
-    # ~TypeVar
-    if isinstance(length, TypeVar):
-        length = None
+    # Union[A, B, ...]
+    if origin is UnionType:
+        resolved = (_resolve_length(arg) for arg in get_args(length))
+        result = set[Any]()
+        for r in resolved:
+            if isinstance(r, set):
+                result |= r
+            else:
+                result.add(r)
+        return result
 
-    if length is None:
+    return length
+
+
+def _validate_length(object: Sequence[Item], length: Any) -> None:
+    if not TypedListConfig.VALIDATE_LENGTH:
         return None
 
+    length = _resolve_length(length)
+
+    # type[Any]
+    if length is Any:
+        return None
+
+    # type[int]; Including <subclass of int>
+    if isinstance(length, type) and issubclass(length, int):
+        return None
+
+    # Should already be disallowed statically, here we just raise a runtime error
+    if length is NoneType:
+        raise LengthError("Invalid length")
+    # [TODO]: Others
+
+    # [NOTE]: From a statical perspective, being strict,
+    # prefer Literal[~int] over ~int, although we allow it here, for now.
+
     actual = len(object)
-    if actual != length:
-        raise LengthError(f"Length mismatch: expected {length}, got {actual}")
+    if isinstance(length, set):
+        if len(length) > 1:  # pyright: ignore[reportUnknownArgumentType]
+            for arg in length:  # pyright: ignore[reportUnknownVariableType]
+                if isinstance(arg, int):
+                    if arg == actual:
+                        break
+
+                ## [TODO]: Similar to the outer validation, so prolly can refactor through a recursive function call
+                # type[Any]
+                if arg is Any:
+                    break
+
+                # type[int]; Including <subclass of int>
+                if isinstance(arg, type) and issubclass(arg, int):
+                    break
+            else:
+                raise LengthError(
+                    f"Length mismatch: expected one of {length}, got {actual}"
+                )
+        elif len(length) == 1:  # pyright: ignore[reportUnknownArgumentType]
+            # Defer to the single case below
+            length = length.pop()  # pyright: ignore[reportUnknownVariableType]
+        else:  # len(length) == 0
+            # This case should prolly never arise? Strictly speaking, statically.
+            return None
+    # (Concrete) int
+    if isinstance(length, int):
+        # This case is just for a minimal error message, we could already handle
+        # it in the `set` case above, provided `_resolve_length` resolves it accordingly.
+        if actual != length:
+            raise LengthError(f"Length mismatch: expected {length}, got {actual}")
+
+    return None  # Fallback
 
 
 def _validate_item(object: Sequence[Item], item_type: Any) -> None:
