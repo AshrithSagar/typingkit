2e23dc2d1cc7dfc65830ad51ff1bb8021a792c5e
Ashrith Sagar <ashrith9sagar@gmail.com>
2026-03-02 01:24:32 +0530

    ✨ Generics

---
+++ src/typed_numpy/_typed/generics.py
@@ -0,0 +1,50 @@
+"""
+Generics
+=======
+"""
+# src/typed_numpy/_typed/generics.py
+
+from types import GenericAlias
+from typing import Any, Generic, Self, TypeVarTuple, Unpack, cast, get_args, get_origin
+
+Ts = TypeVarTuple("Ts", default=Unpack[tuple[Any, ...]])
+
+
+class RuntimeGeneric(Generic[Unpack[Ts]]):
+    @classmethod
+    def __class_getitem__(cls, item: Any, /) -> GenericAlias:
+        # [HACK] Misuses __class_getitem__
+        # See https://docs.python.org/3/reference/datamodel.html#the-purpose-of-class-getitem
+
+        try:
+            ga = cast(GenericAlias, super().__class_getitem__(item))  # type: ignore[misc]
+        except:  # noqa: E722
+            # Fallback if superclass does not implement `__class_getitem__`
+            ga = GenericAlias(cls, item)
+        return _RuntimeGenericAlias.from_generic_alias(ga)
+
+    @classmethod
+    def __pre_new__(cls, alias: GenericAlias, *args: Any, **kwargs: Any) -> Self:
+        return cls(*args, **kwargs)
+
+
+class _RuntimeGenericAlias(GenericAlias):
+    """
+    Deferred RuntimeGeneric constructor.
+    Enables progressive type specialisation, behaving like a type-level curry.
+    """
+
+    @classmethod
+    def from_generic_alias(cls, alias: GenericAlias) -> Self:
+        origin = get_origin(alias)
+        typeargs = get_args(alias)
+        return cls(origin, typeargs)
+
+    def __getitem__(self, typeargs: Any) -> Self:
+        ga = super().__getitem__(typeargs)
+        return type(self).from_generic_alias(ga)
+
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        origin: type[RuntimeGeneric] = get_origin(self)
+        obj = origin.__pre_new__(self, *args, **kwargs)
+        return obj
