9e2b6fd710ff7eeb16f5bf3380c82aab4b6191d9
Ashrith Sagar <ashrith9sagar@gmail.com>
2026-03-07 14:43:36 +0530

    ⏪️ Generics

--- src/typingkit/core/generics.py
+++ src/typingkit/core/generics.py
@@ -17,7 +17,7 @@ from typing import (
     get_origin,
 )
 
-Ts = TypeVarTuple("Ts")
+Ts = TypeVarTuple("Ts", default=Unpack[tuple[Any, ...]])
 
 
 class RuntimeGeneric(Generic[Unpack[Ts]]):
@@ -55,7 +55,7 @@ class _RuntimeGenericAlias(GenericAlias):
         return type(self).from_generic_alias(ga)
 
     def __call__(self, *args: Any, **kwargs: Any) -> Any:
-        origin: type[RuntimeGeneric[Unpack[Ts]]] = get_origin(self)  # type: ignore[valid-type]
+        origin: type[RuntimeGeneric] = get_origin(self)
         obj = origin.__pre_new__(self, *args, **kwargs)
         return obj
 
@@ -64,7 +64,7 @@ class _RuntimeGenericAlias(GenericAlias):
 
 
 def _substitute(tp: Any, mapping: dict[Any, Any]) -> Any:
-    if isinstance(tp, (TypeVar, TypeVarTuple)):
+    if isinstance(tp, TypeVar):
         return mapping.get(tp, tp)
 
     origin = get_origin(tp)
@@ -76,61 +76,14 @@ def _substitute(tp: Any, mapping: dict[Any, Any]) -> Any:
         return tp
 
     new_args = tuple(_substitute(arg, mapping) for arg in args)
-
-    if origin is Unpack:
-        assert len(new_args) == 1
-        new_args = new_args[0]
-        if isinstance(new_args, tuple):
-            return cast(Any, new_args)
-
     return origin[new_args]
-
-
-def _resolve_args(args: tuple[Any, ...], mapping: dict[Any, Any]) -> tuple[Any, ...]:
-    resolved = list[Any]()
-    for arg in args:
-        sub = _substitute(arg, mapping)
-        if isinstance(sub, tuple):
-            resolved.extend(sub)  # pyright: ignore[reportUnknownArgumentType]
-        else:
-            resolved.append(sub)
-    return tuple(resolved)
-
-
-def _build_mapping(
-    parameters: tuple[Any, ...], args: tuple[Any, ...]
-) -> dict[Any, Any]:
-    mapping = dict[Any, Any]()
-
-    arg_i = int(0)
-    for i, param in enumerate(parameters):
-        if isinstance(param, TypeVarTuple):
-            remaining_params = parameters[i + 1 :]
-            remaining_non_variadic = sum(
-                not isinstance(p, TypeVarTuple) for p in remaining_params
-            )
-
-            ts_len = len(args) - arg_i - remaining_non_variadic
-            if ts_len < 0:
-                ts_len = 0
-
-            mapping[param] = args[arg_i : arg_i + ts_len]
-            arg_i += ts_len
-        else:
-            if arg_i >= len(args):
-                raise TypeError("Not enough type arguments")
-
-            mapping[param] = args[arg_i]
-            arg_i += 1
-
-    return mapping
 
 
 def get_runtime_args(tp: Any, cls: type) -> tuple[Any, ...]:
     args = get_args(tp)
 
     parameters: tuple[Any, ...] = getattr(cls, "__parameters__", ())
-    mapping = _build_mapping(parameters, args)
+    mapping = dict(zip(parameters, args))
 
     current_cls = cls
     while True:
@@ -143,11 +96,14 @@ def get_runtime_args(tp: Any, cls: type) -> tuple[Any, ...]:
                 continue
 
             base_args = get_args(base)
-            resolved = _resolve_args(base_args, mapping)
+            resolved = tuple(_substitute(arg, mapping) for arg in base_args)
+
+            if origin is RuntimeGeneric:
+                return resolved
 
             # Propagate mapping
             parent_params = getattr(origin, "__parameters__", ())
-            mapping = _build_mapping(parent_params, resolved)
+            mapping = dict(zip(parent_params, resolved))
             current_cls = origin
             found_base = True
             break
